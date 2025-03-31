620e0e3f77da9f68022d97859e435fd55f889f82
Priyanshu Singh <singhofficial38@gmail.com>
2025-03-31 19:08:07 +0530

    404

---
+++ src/app/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-6xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 text-xl">Sorry, the page you are looking for does not exist.</p>
+    </main>
+  );
+};
+
+export default Custom404;
